b5a3dcc80c3f03cae6cdc19ee06db3eff54458e4
alexpeti <alexandros.peteinaris@hotmail.com>
2022-09-21 22:29:06 +0300

    fixed page essentials, need to add buttons and display the grid correctly

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const container = document.querySelector('#container');
+const blackButton = document.createElement('button');
+const rgbButton = document.createElement('button');
+const eraserButton = document.createElement('button')
+const gridSizeButton = document.createElement('button')
+const buttonsContainer = document.querySelector('buttons')
+
 
 function createDivs(rows,columns) {
   for (let i = 0; i < (rows * columns); i++) {
