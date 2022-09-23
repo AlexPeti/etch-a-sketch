30c3d8e37d57dbaca2bb436489e4175b31520f6d
alexpeti <alexandros.peteinaris@hotmail.com>
2022-09-23 21:18:19 +0300

    fixed the grid-box with js and added a basic style for display, have to work on functionality now

--- script.js
+++ script.js
@@ -1,32 +1,39 @@
-const container = document.querySelector('#container');
-const blackButton = document.createElement('button');
-const rgbButton = document.createElement('button');
-const eraserButton = document.createElement('button');
-const gridSizeButton = document.createElement('button');
-const resetButton = document.createElement('button');
-const buttonsContainer = document.querySelector('buttons');
+function generateGrid(size) {
+let container = document.querySelector('#container');
+container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-
-function createDivs(rows,columns) {
-  for (let i = 0; i < (rows * columns); i++) {
-    const div = document.createElement('div')
-    div.style.border = '1px solid white'
-    container.style.gridRows = `repeat(${rows}, 1fr)`;
-    container.style.gridColumns = `repeat(${columns}, 1fr)`;
-    div.addEventListener('mouseover', changeGridColor)
-    container.appendChild(div).classList.add('box')
-  }
+for (let i = 0; i < 256; i++) {
+  let gridBox = document.createElement('div');
+  gridBox.classList.add('gridbox');
+  gridBox.style.backgroundColor = 'blue';
+  container.insertAdjacentElement("beforeend",gridBox);
 }
 
-function changeGridColor(e) {
-  let color = `rgb(${[0, 0, 0].map(channel => {
-    return Math.floor(Math.random() * 256);
-  }).join(',')})`
-  Object.assign(e.target.style, {
-    backgroundColor : color,
-    opacity : 0.25
-  });
 }
+  
 
-createDivs(16,16)
+  
+
+
+generateGrid(16)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
