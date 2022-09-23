97a3c7798b1ccf70a7a0627f5947c5ff5839c8b0
alexpeti <alexandros.peteinaris@hotmail.com>
2022-09-23 23:53:56 +0300

    made the standard draw function , need to add rgb, eraser and reset buttons plus a size prompt

--- script.js
+++ script.js
@@ -6,18 +6,18 @@ container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 for (let i = 0; i < 256; i++) {
   let gridBox = document.createElement('div');
   gridBox.classList.add('gridbox');
-  gridBox.style.backgroundColor = 'blue';
+  gridBox.addEventListener('mouseover',gridBoxColor);
+  gridBox.style.backgroundColor = 'white';
   container.insertAdjacentElement("beforeend",gridBox);
 }
 
 }
   
-
-  
-
-
 generateGrid(16)
 
+function gridBoxColor() {
+  this.style.backgroundColor = 'black';
+}
 
 
 
