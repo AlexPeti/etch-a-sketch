bd915a22faa97cf450440d53bd0e5ee328d17543
alexpeti <alexandros.peteinaris@hotmail.com>
2022-09-26 18:00:08 +0300

    trying to create rgb color

--- script.js
+++ script.js
@@ -1,36 +1,19 @@
-/*function generateGrid(size) {
-let container = document.querySelector('#container');
-container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-
-for (let i = 0; i < 256; i++) {
-  let gridBox = document.createElement('div');
-  gridBox.classList.add('gridbox');
-  //gridBox.addEventListener('mouseover',gridBoxColor);
-  gridBox.addEventListener('mouseover', drawFunction);
-  gridBox.style.backgroundColor = 'white';
-  container.insertAdjacentElement("beforeend",gridBox);
-}
-}
-  
-generateGrid(16)
-
-document.getElementById('standard').onclick = drawFunction()
-
-function drawFunction() {
-  this.style.backgroundColor = 'black';
-}
-
-function gridBoxColor() {
-  
-  this.style.backgroundColor = 'black';
-}
-*/  ///////////////////// FIRST TRY ///////////////////////////
+let color = 'black';
 
 document.addEventListener('DOMContentLoaded',function(){
+
   console.log('creating simulated universe within simulated universe...');
-  createGrid(32);
+
+  createGrid(16);
+
+  let selectGridSizeButton = document.querySelector('.grid-size-button');
+  selectGridSizeButton.addEventListener('click', function() {
+    let size = getGridSize();
+    createGrid(size);
+  })
+  
 })
+
 
 
 function createGrid(size) {
@@ -41,7 +24,8 @@ function createGrid(size) {
   let gridSize = size * size;
   for(let i = 0; i < gridSize; i++){
     let div = document.createElement('div');
-    div.style.backgroundColor = 'white'
+    div.addEventListener('mouseover', drawGrid);
+    
     container.insertAdjacentElement('beforeend', div)
   }
 }
@@ -49,9 +33,36 @@ function createGrid(size) {
 createGrid();
 
 
+function getGridSize() {
+  let input = prompt('Choose a grid size between 16-100!')
+  let message = document.querySelector('#message')
+  if(input == "") {
+    message.innerHTML = "Please select a grid size";
+  }
+  else if (input < 16 || input > 100) {
+    message.innerHTML = 'Incorrect value, please choose a grid size between 16-100!';
+  }
+  else {
+    message.innerHTML = 'Time to Sketch!'
+  }
+  return input;
+
+}
 
 
+function drawGrid() {
+  if (color == 'black') {
+    this.style.backgroundColor = 'black'
+  }
+  else {
+    this.style.backgroundColor = 'red'
+  }
 
+}
+
+function getColorChoice(colorChoice) {
+  color = colorChoice;
+}
 
 
 
